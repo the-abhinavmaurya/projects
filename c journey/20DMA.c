0ed3ddeed6ff1144e1fd2b8bc8a15eacbca42aaf
Abhinav Maurya <abhinavmaurya7740@gmail.com>
2026-07-30 21:15:15 +0530

    DMA topic

---
+++ c journey/20DMA.c
@@ -0,0 +1,20 @@
+int main()
+{
+int *p; // local variable SMA
+p=malloc(8); //DMA
+
+/*
+calloc is used to make no of blocks with the memory allocation 
+ ## p=calloc(8,4) where 8 is no of blocks and 4 is memory in bytes 
+*/
+
+
+// realloc is used to change or reallocate the memory
+
+double *q;
+q=realloc(p,4);
+
+// free(q);  
+// ### free is used to release the memory
+
+}
