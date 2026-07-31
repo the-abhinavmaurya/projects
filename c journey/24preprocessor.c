305794b4a473bbf71299aa3932924e32114ea7ba
Abhinav Maurya <abhinavmaurya7740@gmail.com>
2026-07-31 23:20:00 +0530

    macropreprocess

---
+++ c journey/24preprocessor.c
@@ -0,0 +1,13 @@
+//deined
+#define PI 3.14
+
+int main()
+{
+  float r;
+  float a;
+  printf("Enter the radius of the circle:\n");
+  scanf("%f",&r);
+  a=PI*r*r;
+printf("Area of circle is:%f",a);
+    
+}
