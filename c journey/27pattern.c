e56f8c15ddba2b4151703bf5ad0ec61b92744c30
Abhinav Maurya <abhinavmaurya7740@gmail.com>
2026-09-17 22:23:10 +0530

    Traingle pattern

---
+++ c journey/27pattern.c
@@ -0,0 +1,42 @@
+/*
+FOR ULTA
+ #include <stdio.h>
+int main()
+{
+
+    int i,j;
+    for(i=5;i>=1;i--)
+        {
+            for(j=1;j<=i;j++) 
+            {
+                printf(" %d",j);
+            }
+         printf("\n");
+            }
+return 0;
+
+    
+} */
+
+
+// FOR SEEDHA
+
+#include <stdio.h>
+int main()
+{
+
+    int i,j;
+    for(i=1;i<=6;i++)
+        {
+            for(j=1;j<=i;j++) 
+            {
+                printf(" %d",j);
+            }
+         printf("\n");
+            }
+return 0;
+
+    
+}
+
+
